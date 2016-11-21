00e4bbfca8ee74afd37585dd15dc8e0687ef93e8
nkkize <nkk.snat@gmail.com>
2016-11-21 12:02:08 +0530

    adding UserRepo

--- acflux-persistence/src/main/java/com/acflux/persistence/repositories/UserRespository.java
+++ acflux-persistence/src/main/java/com/acflux/persistence/repositories/UserRespository.java
@@ -3,13 +3,18 @@
  */
 package com.acflux.persistence.repositories;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.acflux.persistence.entities.User;
 
 /**
  * @author narenderk
  *
  */
 @Repository
-public class UserRespository{
+@Transactional
+public interface UserRespository extends JpaRepository<User, Long>{
 
 }
