1eca6da0969a1fd46706e1b43dbf507c7afe8323
nkkize <nkk.snat@gmail.com>
2016-11-23 19:19:04 +0530

    dependency correction and login API

--- acflux-web/src/main/java/com/acflux/web/config/MvcConfig.java
+++ acflux-web/src/main/java/com/acflux/web/config/MvcConfig.java
@@ -17,10 +17,8 @@ public class MvcConfig extends WebMvcConfigurerAdapter{
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry){
 		registry.addViewController("/").setViewName("login");
-        //registry.addViewController("/login").setViewName("login");
         registry.addViewController("/aboutus").setViewName("aboutus");
         registry.addViewController("/termsConditions").setViewName("termsConditions");
-        registry.addViewController("/home").setViewName("home");
         registry.addViewController("/admin").setViewName("admin");
         registry.addViewController("/admin/management").setViewName("management");		
 	}
--- acflux-web/src/main/java/com/acflux/web/controller/LoginController.java
+++ acflux-web/src/main/java/com/acflux/web/controller/LoginController.java
@@ -3,9 +3,13 @@
  */
 package com.acflux.web.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * @author narenderk
@@ -19,9 +23,19 @@ public class LoginController {
 		return "welcome";
 	}
 	
-	@RequestMapping(value = "/login", method = RequestMethod.POST)
-	public String home(){
-		return "login";
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public ModelAndView login(@RequestParam(value = "error", required = false) String error,
+			@RequestParam(value = "logout", required = false) String logout,
+			HttpServletRequest request) {
+		ModelAndView model = new ModelAndView();
+		if (logout != null) {
+			model.addObject("msg", "logged out successfully.");
+		}
+		if (error != null) {
+			model.addObject("error", "wrong username or password.");
+		}
+		model.setViewName("login");
+		return model;
 	}
 
 }
